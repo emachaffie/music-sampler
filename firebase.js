037b850a27b73189a7f20c78cd2e4511db876300
emachaffie <emachaffie@gmail.com>
2018-07-30 11:30:20 -0400

    Added Firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+
+var config = {
+  apiKey: "AIzaSyAoFFiT-_4LPR-5w_8c7SO5xVNg_urteA8",
+  authDomain: "multilingual-melodies.firebaseapp.com",
+  databaseURL: "https://multilingual-melodies.firebaseio.com",
+  projectId: "multilingual-melodies",
+  storageBucket: "",
+  messagingSenderId: "321486964821"
+};
+firebase.initializeApp(config)
